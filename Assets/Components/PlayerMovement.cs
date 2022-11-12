f2c78d1141024bfb9603bbfa629c0ef1633717b0
kevcch <kevincc@umich.edu>
2022-11-12 11:53:28 -0500

    Added player movement

--- Assets/Components/PlayerMovement.cs
+++ Assets/Components/PlayerMovement.cs
@@ -2,24 +2,36 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(Rigidbody))]
 public class PlayerMovement : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public float jumpForce = 2.0f;
+    public float moveForce = 0.1f;
+    public bool isGrounded;
+
+    void OnCollisionStay()
     {
-        
+        isGrounded = true;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKey(KeyCode.W))
-            transform.localPosition += new Vector3(0.1f, 0, 0.1f);
-        if (Input.GetKey(KeyCode.S))
-            transform.localPosition += new Vector3(-0.1f, 0, -0.1f);
-        if (Input.GetKey(KeyCode.A))
-            transform.localPosition += new Vector3(-0.1f, 0, 0.1f);
-        if (Input.GetKey(KeyCode.D))
-            transform.localPosition += new Vector3(0.1f, 0, -0.1f); ;
+        if (Input.GetKey(KeyCode.W) && isGrounded) {
+            GetComponent<Rigidbody>().AddForce(new Vector3(moveForce, 0, moveForce) * jumpForce, ForceMode.Impulse);
+        }
+        if (Input.GetKey(KeyCode.S) && isGrounded) {
+            GetComponent<Rigidbody>().AddForce(new Vector3(-moveForce, 0, -moveForce) * jumpForce, ForceMode.Impulse);
+        }
+        if (Input.GetKey(KeyCode.A) && isGrounded) {
+            GetComponent<Rigidbody>().AddForce(new Vector3(-moveForce, 0, moveForce) * jumpForce, ForceMode.Impulse);
+        }
+        if (Input.GetKey(KeyCode.D) && isGrounded) { 
+            GetComponent<Rigidbody>().AddForce(new Vector3(moveForce, 0, -moveForce) * jumpForce, ForceMode.Impulse);
+        }
+        if (Input.GetKey(KeyCode.Space) && isGrounded) {
+            GetComponent<Rigidbody>().AddForce(new Vector3(0, 1, 0) * jumpForce, ForceMode.Impulse);
+            isGrounded = false;
+        }
     }
 }
