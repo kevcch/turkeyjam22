d6225782fde4e992d3de51743ff14e3c885eaa4c
kevcch <kevincc@umich.edu>
2022-11-12 13:38:49 -0500

    Another attempt to fix jumping

--- Assets/Components/PlayerMovement.cs
+++ Assets/Components/PlayerMovement.cs
@@ -6,13 +6,19 @@ using UnityEngine;
 [RequireComponent(typeof(Rigidbody))]
 public class PlayerMovement : MonoBehaviour
 {
-    public float jumpForce = 15.0f;
+    public float jumpForce = 25.0f;
     public float moveForce = 2.0f;
+    public bool isColliding;
     public bool isGrounded;
 
     private void Start()
     {
+        isColliding = false;
         isGrounded = false;
+    }
+    void OnCollisionStay()
+    {
+        isColliding = true;
     }
 
     // Update is called once per frame
@@ -23,24 +29,24 @@ public class PlayerMovement : MonoBehaviour
         if (Physics.Raycast(transform.position, Vector3.down, out hit, distance))
         {
             Debug.Log(transform.position.y - hit.point.y);
-            if (transform.position.y-hit.point.y < 0.5)
+            if (transform.position.y-hit.point.y < 0.3)
                 isGrounded = true;
         }
 
 
-        if (Input.GetKey(KeyCode.W) && isGrounded) {
+        if (Input.GetKey(KeyCode.W) && isGrounded && isColliding) {
             GetComponent<Rigidbody>().AddForce(new Vector3(moveForce, 0, 0), ForceMode.Impulse);
         }
-        if (Input.GetKey(KeyCode.S) && isGrounded) {
+        if (Input.GetKey(KeyCode.S) && isGrounded && isColliding) {
             GetComponent<Rigidbody>().AddForce(new Vector3(-moveForce, 0, 0), ForceMode.Impulse);
         }
-        if (Input.GetKey(KeyCode.A) && isGrounded) {
+        if (Input.GetKey(KeyCode.A) && isGrounded && isColliding) {
             GetComponent<Rigidbody>().AddForce(new Vector3(0, 0, moveForce), ForceMode.Impulse);
         }
-        if (Input.GetKey(KeyCode.D) && isGrounded) { 
+        if (Input.GetKey(KeyCode.D) && isGrounded && isColliding) { 
             GetComponent<Rigidbody>().AddForce(new Vector3(0, 0, -moveForce), ForceMode.Impulse);
         }
-        if (Input.GetKey(KeyCode.Space) && isGrounded) {
+        if (Input.GetKey(KeyCode.Space) && isGrounded && isColliding) {
             GetComponent<Rigidbody>().AddForce(new Vector3(0, 1, 0) * jumpForce, ForceMode.Impulse);
             isGrounded = false;
         }
