1e341ac9a1785a5930070787d333b10e21b448a4
Jasmine Li <jasmineli106@gmail.com>
2022-11-12 13:07:02 -0500

    Grabbable objects

---
+++ Assets/Components/GrabObjects.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GrabObjects : MonoBehaviour
+{
+    private Transform playerParent;
+    private Transform player;
+    private bool grabbed = false;
+    private Vector3 offset;
+    private GrabbedManager grabManager;
+
+    void Start()
+    {
+        playerParent = GameObject.FindWithTag("Player").transform;
+        player = playerParent.transform.GetChild(0);
+        grabManager = player.GetComponent<GrabbedManager>();
+        
+    }
+
+    void Update() {
+        if(grabManager.grabbed) {
+            transform.position = player.position + offset;
+        }
+    }
+
+    void OnCollisionEnter(Collision collision) {
+        if(collision.gameObject.tag == "Player" && !grabManager.grabbed) {
+            grabManager.grabbed = true;
+            offset = transform.position - collision.transform.position;
+        }
+    }
+}
