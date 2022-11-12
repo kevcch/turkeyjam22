e450bb062ab47ade50ee1329048c5d4da9fd5c7a
kevcch <kevincc@umich.edu>
2022-11-12 13:10:39 -0500

    Updated Player movement and added cinemachine camera

--- Assets/Components/PlayerMovement.cs
+++ Assets/Components/PlayerMovement.cs
@@ -5,8 +5,8 @@ using UnityEngine;
 [RequireComponent(typeof(Rigidbody))]
 public class PlayerMovement : MonoBehaviour
 {
-    public float jumpForce = 2.0f;
-    public float moveForce = 0.1f;
+    public float jumpForce = 15.0f;
+    public float moveForce = 2.0f;
     public bool isGrounded;
 
     void OnCollisionStay()
@@ -18,16 +18,16 @@ public class PlayerMovement : MonoBehaviour
     void Update()
     {
         if (Input.GetKey(KeyCode.W) && isGrounded) {
-            GetComponent<Rigidbody>().AddForce(new Vector3(moveForce, 0, moveForce) * jumpForce, ForceMode.Impulse);
+            GetComponent<Rigidbody>().AddForce(new Vector3(moveForce, 0, 0), ForceMode.Impulse);
         }
         if (Input.GetKey(KeyCode.S) && isGrounded) {
-            GetComponent<Rigidbody>().AddForce(new Vector3(-moveForce, 0, -moveForce) * jumpForce, ForceMode.Impulse);
+            GetComponent<Rigidbody>().AddForce(new Vector3(-moveForce, 0, 0), ForceMode.Impulse);
         }
         if (Input.GetKey(KeyCode.A) && isGrounded) {
-            GetComponent<Rigidbody>().AddForce(new Vector3(-moveForce, 0, moveForce) * jumpForce, ForceMode.Impulse);
+            GetComponent<Rigidbody>().AddForce(new Vector3(0, 0, moveForce), ForceMode.Impulse);
         }
         if (Input.GetKey(KeyCode.D) && isGrounded) { 
-            GetComponent<Rigidbody>().AddForce(new Vector3(moveForce, 0, -moveForce) * jumpForce, ForceMode.Impulse);
+            GetComponent<Rigidbody>().AddForce(new Vector3(0, 0, -moveForce), ForceMode.Impulse);
         }
         if (Input.GetKey(KeyCode.Space) && isGrounded) {
             GetComponent<Rigidbody>().AddForce(new Vector3(0, 1, 0) * jumpForce, ForceMode.Impulse);
