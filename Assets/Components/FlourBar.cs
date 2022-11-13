a07d519d2f19cf9c2e80f47447d0b528e65c7695
Jasmine Li <jasmineli106@gmail.com>
2022-11-13 16:42:39 -0500

    Flour death

--- Assets/Components/FlourBar.cs
+++ Assets/Components/FlourBar.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class FlourBar : MonoBehaviour
 {
@@ -9,5 +10,14 @@ public class FlourBar : MonoBehaviour
     void Update()
     {
         rt.sizeDelta = new Vector2(5f * FlourPointTracker.numFlourPoints, 20f);
+        if(FlourPointTracker.numFlourPoints <= 0) {
+            StartCoroutine(Death());
+        }
+    }
+
+    IEnumerator Death() {
+        yield return new WaitForSeconds(1f);
+        FlourPointTracker.numFlourPoints = 100;
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
     }
 }
