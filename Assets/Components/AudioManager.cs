14f5ee4e426a448a26a14b53f9c9a0dc15531abb
loganhughesr <hulogan@umich.edu>
2022-11-13 11:02:12 -0500

    audio manager in cutscene 0

--- Assets/Components/AudioManager.cs
+++ Assets/Components/AudioManager.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class AudioManager : MonoBehaviour
 {
+    public static AudioManager am;
+
     public Sound[] sounds;
 
     void Awake()
@@ -17,15 +19,35 @@ public class AudioManager : MonoBehaviour
             s.source.pitch = s.pitch;
             s.source.loop = s.loop;
         }
+
+        if (GameObject.Find("Audio Manager") != null)
+            Destroy(this.gameObject);
+
+        DontDestroyOnLoad(this.gameObject);
     }
 
+    // Play sound by name
     public void Play(string name)
     {
         Array.Find(sounds, sound => sound.name == name).source.Play();
     }
 
-    public void PlayGroupRandomPitch(string name)
+    // Play sound by name with random pitch
+    public void PlayPitchRandom(string name,
+        float minPitch = 0.7f, float maxPitch = 1.3f)
     {
+        AudioSource s = Array.Find(sounds, sound => sound.name == name).source;
+        s.pitch = UnityEngine.Random.Range(minPitch, maxPitch);
+        s.Play();
+    }
 
+    // Play sounds from group randomly with random pitch
+    public void PlayGroupRandomPitch(string group,
+        float minPitch = 0.8f, float maxPitch = 1.2f)
+    {
+        var g = Array.FindAll(sounds, sound => sound.group == group);
+        int index = UnityEngine.Random.Range(0, g.Length);
+        g[index].source.pitch = UnityEngine.Random.Range(minPitch, maxPitch);
+        g[index].source.Play();
     }
 }
