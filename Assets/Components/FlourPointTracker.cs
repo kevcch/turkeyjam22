2b7a668220800ff143fd6846a4836072992a7027
kevcch <kevincc@umich.edu>
2022-11-13 14:51:38 -0500

    Make flour points more forgiving and move milk to correct place

--- Assets/Components/FlourPointTracker.cs
+++ Assets/Components/FlourPointTracker.cs
@@ -5,5 +5,5 @@ using UnityEngine;
 public class FlourPointTracker : MonoBehaviour
 {
     public static float numFlourPoints = 100f;
-    public static float point_multiplier = 0.02f;
+    public static float point_multiplier = 0.01f;
 }
