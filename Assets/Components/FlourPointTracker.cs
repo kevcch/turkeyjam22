98a2511fc456c056457fabf856237494a7f685d4
kevcch <kevincc@umich.edu>
2022-11-12 20:42:47 -0500

    Flour tracker and player movement toggle for 45 degrees

---
+++ Assets/Components/FlourPointTracker.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlourPointTracker : MonoBehaviour
+{
+    public static float numFlourPoints = 100f;
+    public static float point_multiplier = 0.01f;
+}
